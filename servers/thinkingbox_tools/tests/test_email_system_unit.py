be9e157bd5b4615297182278db12609a91f01b24
ltsai-microsoft <ltsai@microsoft.com>
2026-05-11 15:27:08 -0700

    docs: add copyright and license headers to multiple files

--- servers/thinkingbox_tools/tests/test_email_system_unit.py
+++ servers/thinkingbox_tools/tests/test_email_system_unit.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import pytest
 
 from thinkingbox_tools.toolslib.email_system import (
--- servers/thinkingbox_tools/thinkingbox_tools/mcp_email_system.py
+++ servers/thinkingbox_tools/thinkingbox_tools/mcp_email_system.py
@@ -1,3 +1,6 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
 import json
 import traceback
 from typing import Annotated, Literal
